2981245077311557fd632c3abddb9d99b0d985ee
B͈̤̖̪̪̱ͅl̯̯̮̼͎̬͚̳̩̖̲u̜̼͉͈̠b͙̬̘̙̱̗̲͙b͍̞̬̬͓̼l̰̪͖̯̼̟̟͈̖͕̜̱̜ͅl̻̗͔̝̭̰͚͇̯̥ <34196146+blubbll@users.noreply.github.com>
2018-09-23 12:42:00 +0200

    doesnt work under ie7

--- bootstrap-cookie-consent.js
+++ bootstrap-cookie-consent.js
@@ -1,5 +1,5 @@
 
-if(window.ie && document.documentMode > 5 || !window.ie){
+if(window.ie && document.documentMode > 7 || !window.ie){
 // Creare's 'Implied Consent' EU Cookie Law Banner v:2.4
 // Conceived by Robert Kent, James Bavington & Tom Foyster
 // Modified by Simon Freytag for syntax, namespace, jQuery and Bootstrap
